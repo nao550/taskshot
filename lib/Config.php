15b6f8a18a756075ff43e6cc8f7bd205cd998dff
Naoki Ikeno <nao@kyo-to.net>
2018-09-27 13:57:27 +0900

    Server switch.

--- lib/Config.php
+++ lib/Config.php
@@ -3,16 +3,23 @@ namespace ftech;
 
 class Config
 {
-  private $config;
-  public  $smarty;
-  private $dbsv = 'localhost';
-  private $dbnm = 'taskshot';
-  private $dbuser = 'taskshot';
-  private $dbpass = 'qbwNnfQ#Wjfc9S7Ww';
-  private $homedir = __DIR__;
+      private $config;
+      public  $smarty;
+      private $dbsv = 'localhost';
+      private $dbnm = 'taskshot';
+      private $dbuser = 'taskshot';
+      private $dbpass = 'qbwNnfQ#Wjfc9S7Ww';
+      private $homedir = __DIR__;
 
   function __construct($Config = ''){
     $this->config = $Config;
+
+    if ( $_SERVER['SERVER_NAME'] == "taskshot.info" ){
+      $this->dbnm = 'taskshot';
+    } else {
+      // 開発用サーバ
+      $this->dbnm = 'taskshotdev';
+    }
   }
 
   public function getDsn(){
